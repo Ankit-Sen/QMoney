449215e31a8a7f014453588b3d36001d483b92ba
Ankit Sen <ankitsenchowdhury@gmail.com>
2024-02-13 01:33:34 +0530

    first commit

---
+++ src/test/java/com/example/QMoney/QMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
